47b254957681da0993c0006dd1190f97e80fb113
xiaowei <1412157658@qq.com>
2019-06-29 10:40:03 +0800

    一级路由的搭建

---
+++ src/containers/main/main.jsx
@@ -0,0 +1,12 @@
+// 主界面路由
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        主页面
+      </div>
+    )
+  }
+}
